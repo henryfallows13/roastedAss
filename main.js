92112da71f3fc4bc17f31f52d65a1e2c1d00c02a
Kaleemashraf21 <71667955+Kaleemashraf21@users.noreply.github.com>
2020-10-22 00:50:29 +0100

    minor changes
    fixed price, spelling mistakes, box description also created a new logo

--- main.js
+++ main.js
@@ -26,46 +26,46 @@ var total_price = 0;
 const table_number = document.getElementById("table_number");
 
 //setting drink prices
-const c1_price = 2;
-const c2_price = 2.50;
-const c3_price = 1.50;
-const c4_price = 1;
+const c1_price = 15.75;
+const c2_price = 15.75;
+const c3_price = "13.50";
+const c4_price = "11.50";
 
-const t1_price = 2;
-const t2_price = 3;
-const t3_price = 1.50;
-const t4_price = 3;
-const t5_price = 2.50;
-const t6_price = 4;
+const t1_price = "11.50";
+const t2_price = "11.50";
+const t3_price = "11.50";
+const t4_price = "13.50";
+const t5_price = 15.75;
+const t6_price = "11.50";
 
-const s1_price = 0.50;
-const s2_price = 0.50;
+const s1_price = 10.75;
+const s2_price = 10.75;
 
 
 //changing name to price when hover
 coffee1.addEventListener("mouseover", function() {
-    this.textContent = `$${c1_price}`;
+    this.textContent = `AED ${c1_price}`;
   })
 coffee1.addEventListener("mouseout", function() {
     this.textContent = "ADD TO ORDER";
   })
 
 coffee2.addEventListener("mouseover", function() {
-    this.textContent = `$${c2_price}`;
+    this.textContent = `AED ${c2_price}`;
   })
 coffee2.addEventListener("mouseout", function() {
     this.textContent = "ADD TO ORDER";
   })
 
 coffee3.addEventListener("mouseover", function() {
-    this.textContent = `$${c3_price}`;
+    this.textContent = `AED ${c3_price}`;
   })
 coffee3.addEventListener("mouseout", function() {
     this.textContent = "ADD TO ORDER";
   })
 
 coffee4.addEventListener("mouseover", function() {
-    this.textContent = `$${c4_price}`;
+    this.textContent = `AED ${c4_price}`;
   })
 coffee4.addEventListener("mouseout", function() {
     this.textContent = "ADD TO ORDER";
@@ -73,42 +73,42 @@ coffee4.addEventListener("mouseout", function() {
 
 
 tea1.addEventListener("mouseover", function() {
-    this.textContent = `$${t1_price}`;
+    this.textContent = `AED ${t1_price}`;
   })
 tea1.addEventListener("mouseout", function() {
     this.textContent = "ADD TO ORDER";
   })
 
 tea2.addEventListener("mouseover", function() {
-    this.textContent = `$${t2_price}`;
+    this.textContent = `AED ${t2_price}`;
   })
 tea2.addEventListener("mouseout", function() {
     this.textContent = "ADD TO ORDER";
   })
 
 tea3.addEventListener("mouseover", function() {
-    this.textContent = `$${t3_price}`;
+    this.textContent = `AED ${t3_price}`;
   })
 tea3.addEventListener("mouseout", function() {
     this.textContent = "ADD TO ORDER";
   })
 
 tea4.addEventListener("mouseover", function() {
-    this.textContent = `$${t4_price}`;
+    this.textContent = `AED ${t4_price}`;
   })
 tea4.addEventListener("mouseout", function() {
     this.textContent = "ADD TO ORDER";
   })
 
 tea5.addEventListener("mouseover", function() {
-    this.textContent = `$${t5_price}`;
+    this.textContent = `AED ${t5_price}`;
   })
 tea5.addEventListener("mouseout", function() {
     this.textContent = "ADD TO ORDER";
   })
 
 tea6.addEventListener("mouseover", function() {
-    this.textContent = `$${t6_price}`;
+    this.textContent = `AED ${t6_price}`;
   })
 tea6.addEventListener("mouseout", function() {
     this.textContent = "ADD TO ORDER";
@@ -116,14 +116,14 @@ tea6.addEventListener("mouseout", function() {
 
 
 soft1.addEventListener("mouseover", function() {
-    this.textContent = `$${s1_price}`;
+    this.textContent = `AED ${s1_price}`;
   })
 soft1.addEventListener("mouseout", function() {
     this.textContent = "ADD TO ORDER";
   })
 
 soft2.addEventListener("mouseover", function() {
-    this.textContent = `$${s2_price}`;
+    this.textContent = `AED ${s2_price}`;
   })
 soft2.addEventListener("mouseout", function() {
     this.textContent = "ADD TO ORDER";
@@ -132,85 +132,85 @@ soft2.addEventListener("mouseout", function() {
 //adding to order array
 
 coffee1.addEventListener("click",()=>{
-    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Capuccino($${c1_price})`));
+    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Capuccino(AED ${c1_price})`));
     total_price = total_price + c1_price;
-    order_array.push(`Capuccino($${c1_price})`);
+    order_array.push(`Capuccino(AED ${c1_price})`);
 })
 
 coffee2.addEventListener("click",()=>{
-    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Latte($${c2_price})`));
+    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Latte(AED ${c2_price})`));
     total_price = total_price + c2_price;
-    order_array.push(`Latte($${c2_price})`);
+    order_array.push(`Latte(AED ${c2_price})`);
 })
 
 coffee3.addEventListener("click",()=>{
-    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Americano($${c3_price})`));
+    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Americano(AED ${c3_price})`));
     total_price = total_price + c3_price;
-    order_array.push(`Americano($${c3_price})`);
+    order_array.push(`Americano(AED ${c3_price})`);
 })
 
 coffee4.addEventListener("click",()=>{
-    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Esspresso($${c4_price})`));
+    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Esspresso(AED ${c4_price})`));
     total_price = total_price + c4_price;
-    order_array.push(`Esspresso($${c4_price})`);
+    order_array.push(`Esspresso(AED ${c4_price})`);
 })
 
 
 //showing order on order summary and creating array w them
 tea1.addEventListener("click",()=>{
-    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Brewed Tea($${t1_price})`));
+    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Brewed Tea(AED ${t1_price})`));
     total_price = total_price + t1_price;
-    order_array.push(`Brewed Tea($${t1_price})`);
+    order_array.push(`Brewed Tea(AED ${t1_price})`);
 })
 
 tea2.addEventListener("click",()=>{
-    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Macha Green Tea($${t2_price})`));
+    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Macha Green Tea(AED ${t2_price})`));
     total_price = total_price + t2_price;
-    order_array.push(`Macha Green Tea($${t2_price})`);
+    order_array.push(`Macha Green Tea(AED ${t2_price})`);
 })
 
 tea3.addEventListener("click",()=>{
-    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Mint Tea($${t3_price})`));
+    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Mint Tea(AED ${t3_price})`));
     total_price = total_price + t3_price;
-    order_array.push(`Mint Tea($${t3_price})`);
+    order_array.push(`Mint Tea(AED ${t3_price})`);
 })
 
 tea4.addEventListener("click",()=>{
-    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Chi Latte($${t4_price})`));
+    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Chi Latte(AED ${t4_price})`));
     total_price = total_price + t4_price;
-    order_array.push(`Chi Latte($${t4_price})`);
+    order_array.push(`Chi Latte(AED ${t4_price})`);
 })
 
 tea5.addEventListener("click",()=>{
-    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Iced Chi Tea Latte($${t5_price})`));
+    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Iced Chi Tea Latte(AED ${t5_price})`));
     total_price = total_price + t5_price;
-    order_array.push(`Iced Chi Tea Latte($${t5_price})`);
+    order_array.push(`Iced Chi Tea Latte(AED ${t5_price})`);
 })
 
 tea6.addEventListener("click",()=>{
-    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Shaked Peach Citrus Tea($${t6_price})`));
+    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Shaked Peach Citrus Tea(AED ${t6_price})`));
     total_price = total_price + t6_price;
-    order_array.push(`Shaked Peach Citrus Tea($${t6_price})`);
+    order_array.push(`Shaked Peach Citrus Tea(AED ${t6_price})`);
 })
 
 
 
 soft1.addEventListener("click",()=>{
-    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Fanta($${s1_price})`));
+    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Fanta(AED ${s1_price})`));
     total_price = total_price + s1_price;
-    order_array.push(`Fanta($${s1_price})`);
+    order_array.push(`Fanta(AED ${s1_price})`);
 })
 
 soft2.addEventListener("click",()=>{
-    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Coka-cola($${s2_price})`));
+    order_list.appendChild(document.createElement(`li`)).appendChild(document.createTextNode(`Coka-cola(AED ${s2_price})`));
     total_price = total_price + s2_price;
-    order_array.push(`Coka-cola($${s2_price})`);
+    order_array.push(`Coka-cola(AED ${s2_price})`);
 })
 
 //displayin pcire when hover over pay button and says succesfull payment when pressed 
 
 pay_button.addEventListener("mouseover", function() {
-    this.textContent = `$${total_price}`;
+    this.textContent = `AED ${total_price}`;
   })
 pay_button.addEventListener("mouseout", function() {
     this.textContent = "PAY";
